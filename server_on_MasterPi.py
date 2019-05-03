13da267417e62faf2b5d43ab8db42bd86379f82c
Jait Jacob <jait.jacob@worldreach.local>
2019-05-03 14:29:15 +1000

    tranforming server to OO by introducing class

--- server_on_MasterPi.py
+++ server_on_MasterPi.py
@@ -4,17 +4,21 @@
 
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Considering non-privileged ports are > 1023
+class Server:
+    def server_listening(self):
+        HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+        PORT = 65432        # Considering non-privileged ports are > 1023
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+
+    

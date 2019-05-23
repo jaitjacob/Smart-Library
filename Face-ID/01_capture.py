4f2a11d8093412068ab4629ddf9d491858f13e66
Cliffy25 <lukeclifo@gmail.com>
2019-05-23 12:34:13 +0100

    Slight edits made to facial recognition files

--- Face-ID/01_capture.py
+++ Face-ID/01_capture.py
@@ -13,53 +13,63 @@ import cv2
 import os
 import argparse
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--name", required = True,
-    help="The name/id of this person you are recording")
-ap.add_argument("-i", "--dataset", default = "dataset",
-    help="path to input directory of faces + images")
-args = vars(ap.parse_args())
+class Camera:
+	def runCamera(self):
+		# construct the argument parser and parse the arguments
 
-# use name as folder name
-name = args["name"]
-folder = "./dataset/{}".format(name)
+		ap = argparse.ArgumentParser()
+		ap.add_argument("-n", "--name", required = True,
+			help="The name/id of this person you are recording")
+		ap.add_argument("-i", "--dataset", default = "dataset",
+			help="path to input directory of faces + images")
+		args = vars(ap.parse_args())
 
-# Create a new folder for the new name
-if not os.path.exists(folder):
-    os.makedirs(folder)
+		# use name as folder name
 
-# Start the camera
-cam = cv2.VideoCapture(0)
-# Set video width
-cam.set(3, 640)
-# Set video height
-cam.set(4, 480)
-# Get the pre-built classifier that had been trained on 3 million faces
-face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+		name = args["name"]
+		folder = "./dataset/{}".format(name)
 
-img_counter = 0
-while img_counter <= 10:
-    key = input("Press q to quit or ENTER to continue: ")
-    if key == "q":
-        break
-    
-    ret, frame = cam.read()
-    if not ret:
-        break
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+		# Create a new folder for the new name
+		if not os.path.exists(folder):
+			os.makedirs(folder)
 
-    if(len(faces) == 0):
-        print("No face detected, please try again")
-        continue
-    
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        img_name = "{}/{:04}.jpg".format(folder, img_counter)
-        cv2.imwrite(img_name, frame[y : y + h, x : x + w])
-        print("{} written!".format(img_name))
-        img_counter += 1
 
-cam.release()
+		# Start the camera
+		cam = cv2.VideoCapture(0)
+		# Set video width
+		cam.set(3, 1280)
+		# Set video height
+		cam.set(4, 720)
+		# Get the pre-built classifier that had been trained on 3 million faces
+		face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+		img_counter = 0
+		while img_counter <= 10:
+			key = input("Press q to quit or ENTER to continue: ")
+			if key == "q":
+				break
+
+			ret, frame = cam.read()
+			if not ret:
+				break
+
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+			if(len(faces) == 0):
+				print("No face detected, please try again")
+				continue
+
+			for (x, y, w, h) in faces:
+				cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+				img_name = "{}/{:04}.jpg".format(folder, img_counter)
+				cv2.imwrite(img_name, frame[y : y + h, x : x + w])
+				print("{} written!".format(img_name))
+				img_counter += 1
+
+		cam.release()
+
+if __name__ == "__main__":
+	camera = Camera()
+	camera.runCamera()
+

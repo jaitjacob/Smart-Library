3e591555de7f3beb64234ceaf9f9e2140abf34bb
Jaeneung Han <s3666994@student.rmit.edu.au>
2019-05-25 23:50:20 +1000

    Added clouddb module

---
+++ mp/clouddb.py
@@ -0,0 +1,40 @@
+import MySQLdb
+
+
+class CloudDB:
+    HOST = "35.197.161.11"
+    USER = "root"
+    PASSWORD = "468yANs7gktk5DdL"
+    DATABASE = "lms"
+
+    def __init__(self, connection=None):
+        if (connection == None):
+            connection = MySQLdb.connect(CloudDB.HOST, CloudDB.USER,
+                                         CloudDB.PASSWORD, CloudDB.DATABASE)
+        self.connection = connection
+
+    def close(self):
+        self.connection.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def search_available(self, title: str, author: str, date: str):
+        with self.connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM Book WHERE BookID NOT IN ( "
+                           "SELECT DISTINCT BookID FROM BookBorrowed "
+                           "WHERE Status='borrowed' ) "
+                           "AND Title LIKE :title "
+                           "AND Author LIKE :author "
+                           "AND Date LIKE :date",
+                           {"title": title, "author": author, "date": date})
+            return cursor.fetchall()
+
+
+db = CloudDB()
+
+for row in db.search_available("TRANSMISSION", "", ""):
+    print(row)

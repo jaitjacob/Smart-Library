5a71c95d870d5df0397f0cdd561b45c955a621d3
Jaeneung Han <s3666994@student.rmit.edu.au>
2019-05-26 14:40:28 +1000

    Added methods for borrowing and returning books

--- mp/clouddb.py
+++ mp/clouddb.py
@@ -1,4 +1,5 @@
 import MySQLdb
+from datetime import datetime
 
 
 class CloudDB:
@@ -22,13 +23,13 @@ class CloudDB:
     def __exit__(self, type, value, traceback):
         self.close()
 
-    def search_available(self, title: str, author: str, date: str):
+    def search_available(self, title: str, author: str):
         with self.connection.cursor() as cursor:
             cursor.execute("SELECT * FROM Book WHERE BookID NOT IN ( "
                            "SELECT DISTINCT BookID FROM BookBorrowed "
                            "WHERE Status='borrowed' ) "
-                           "AND Title LIKE %s",
-				('%' + title + '%',))
+                           "AND Title LIKE %s "
+                           "AND Author LIKE %s ", ('%' + title + '%', '%' + author + '%'))
             return cursor.fetchall()
 
     def list_titles(self):
@@ -36,8 +37,27 @@ class CloudDB:
             cursor.execute("SELECT * FROM Book ")
             return cursor.fetchall()
 
+    def borrow_book(self, userid: int, bookid: int ):
+        borrowdate = datetime.today()
+
+        with self.connection.cursor() as cursor:
+            cursor.execute("INSERT INTO BookBorrowed (LmsUserID, BookID, Status, BorrowedDate)"
+                           "VALUES (%s, %s, %s, %s", (userid, bookid, 'borrowed', borrowdate.strftime("%Y-%m-%d")))
+            return cursor.fetchall()
+
+    def return_book(self, bookid: int):
+        returndate = datetime.today()
+
+        with self.connection.cursor() as cursor:
+            cursor.execute("UPDATE "
+                           "SET Status = %s, ReturnedDate %s "
+                           "WHERE BookID = %s", ('returned', returndate.strftime("%Y-%m-%d"), bookid))
+            return cursor.fetchall()
+
+
+
 
 db = CloudDB()
 
-for row in db.search_available("Transmission", " ", " "):
+for row in db.search_available("Transmission", "", ""):
     print(row)
